c4f4660f677f8b4205afde9d06bc1182f0007306
MarSavNtt <135595211+MarSavNtt@users.noreply.github.com>
2024-05-22 14:53:32 +0200

    Update main.py

--- Progetto/BqAndStorageChecker/main.py
+++ Progetto/BqAndStorageChecker/main.py
@@ -1,6 +1,6 @@
 import os
 
-credentials_path = "credentials/training-gcp-309207-ddb500a2fcb9.json"
+credentials_path = "credentials/name"
 
 # Imposta la variabile d'ambiente GOOGLE_APPLICATION_CREDENTIALS
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
@@ -10,11 +10,11 @@ from classes.BqChecker import BigQueryChecker
 from classes.StorageChecker import StorageChecker
 
 def main():
-    bucket_name = 'bucketragazzidigcp'
+    bucket_name = 'bucketgcp'
     blob_name = 'company.csv'
     storage_checker = StorageChecker()
     print(storage_checker.blob_exists(bucket_name=bucket_name, blob_name=blob_name))
-    dataset_id = 'excellenceacademy_mario_saviano'
+    dataset_id = 'id-dataset'
     table_id = 'corso_t'
     big_query_checker = BigQueryChecker(dataset_id=dataset_id)
     print(big_query_checker.table_exists(table_id=table_id))
